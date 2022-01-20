f913788bd5010bb3673c3d059612e5ec6b99173b
ddanyaa <davidenko.dv@students.dvfu.ru>
2022-01-20 22:35:45 +1000

    lab5 update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+"""REST API"""
 from io import BytesIO
-
+from functools import wraps
 from flask import Flask, request, jsonify, make_response, send_file
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
-from functools import wraps
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'do1olfi1bk2hs3scras4dkdaey'
@@ -14,18 +15,21 @@ database = SQLAlchemy(app)
 
 
 class Users(database.Model):
+    """Создание таблицы пользователя"""
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(50))
     password = database.Column(database.String(50))
 
 
 class Tasks(database.Model):
+    """Создание таблицы для хранения задач"""
     id = database.Column(database.Integer, primary_key=True)
     task = database.Column(database.String(100), nullable=False)
     user = database.Column(database.String(50), nullable=False)
 
 
 class Files(database.Model):
+    """Создание таблицы для хранения файлов"""
     id = database.Column(database.Integer, primary_key=True)
     file_name = database.Column(database.String(100), nullable=False)
     file_size = database.Column(database.String(50), nullable=False)
@@ -33,11 +37,10 @@ class Files(database.Model):
     user = database.Column(database.String(50), nullable=False)
 
 
-def token_required(f):
-    @wraps(f)
+def token_required(temp):
+    @wraps(temp)
     def decorator(secret_key=app.config['SECRET_KEY'], *args, **kwargs):
         token = None
-
         if 'x-access-tokens' in request.headers:
             token = request.headers['x-access-tokens']
 
@@ -50,18 +53,20 @@ def token_required(f):
         except:
             return jsonify({'message': 'token is invalid'})
 
-        return f(current_user, *args, **kwargs)
+        return temp(current_user, *args, **kwargs)
 
     return decorator
 
 
 @app.route('/')
 def index():
+    """Метод отображения главной страницы"""
     return 'TodoList'
 
 
 @app.route('/user', methods=['POST'])
 def signup_user():
+    """Метод для регистрации"""
     username = request.form['username']
     password = request.form['password']
     hashed_password = generate_password_hash(password, method='sha256')
@@ -75,6 +80,7 @@ def signup_user():
 
 @app.route('/login', methods=['POST'])
 def login_user():
+    """Метод для авторизации"""
     username = request.form['username']
     password = request.form['password']
 
@@ -86,12 +92,14 @@ def login_user():
             {'name': user.name}, app.config['SECRET_KEY'], algorithm="HS256")
         return jsonify({'token': token})
 
-    return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+    return make_response('could not verify', 401,
+                         {'WWW.Authentication': 'Basic realm: "login required"'})
 
 
 @app.route('/todo', methods=['GET'])
 @token_required
 def task_view(current_user):
+    """Отображение задач пользователя"""
     tasks = Tasks.query.filter(Tasks.user == current_user.name)
     result = []
     for task in tasks:
@@ -103,6 +111,7 @@ def task_view(current_user):
 @app.route('/todo', methods=['POST'])
 @token_required
 def create_task(current_user):
+    """Создание задачи пользователя"""
     task = request.form['task']
     new_task = Tasks(task=task, user=current_user.name)
     database.session.add(new_task)
@@ -110,10 +119,11 @@ def create_task(current_user):
     return jsonify({'message': 'new task created'})
 
 
-@app.route('/todo/<int:id>', methods=['PUT'])
+@app.route('/todo/<int:id_task>', methods=['PUT'])
 @token_required
-def update_task(current_user, id):
-    element = Tasks.query.filter(Tasks.id == id, Tasks.user == current_user.name).first()
+def update_task(current_user, id_task):
+    """Обновление задачи пользователя"""
+    element = Tasks.query.filter(Tasks.id == id_task, Tasks.user == current_user.name).first()
     new_task = request.form['task']
     if not element:
         return make_response('Task not found', 401)
@@ -122,10 +132,11 @@ def update_task(current_user, id):
     return jsonify({'message': 'Task updated'})
 
 
-@app.route('/todo/<int:id>', methods=['DELETE'])
+@app.route('/todo/<int:id_task>', methods=['DELETE'])
 @token_required
-def deleting_task(current_user, id):
-    element = Tasks.query.filter(Tasks.id == id, Tasks.user == current_user.name).first()
+def deleting_task(current_user, id_task):
+    """Удаление задачи пользователя"""
+    element = Tasks.query.filter(Tasks.id == id_task, Tasks.user == current_user.name).first()
     if not element:
         return make_response('Task not found', 401)
     database.session.delete(element)
@@ -136,6 +147,7 @@ def deleting_task(current_user, id):
 @app.route('/files', methods=['GET'])
 @token_required
 def file_browsing(current_user):
+    """Отображение файлов пользователя"""
     files = Files.query.filter(Files.user == current_user.name)
     result = []
     for file in files:
@@ -149,6 +161,7 @@ def file_browsing(current_user):
 @app.route('/files', methods=['POST'])
 @token_required
 def fileupload(current_user):
+    """Добавление нового файла"""
     new_file = request.files['file']
     data = new_file.read()
     file_size = str(len(data))
@@ -164,6 +177,7 @@ def fileupload(current_user):
 @app.route('/files/<string:file_name>', methods=['GET'])
 @token_required
 def download_file(current_user, file_name):
+    """Скачивание файла"""
     file = Files.query.filter(Files.file_name == file_name,
                               Files.user == current_user.name).first()
     if not file:
@@ -174,6 +188,7 @@ def download_file(current_user, file_name):
 @app.route('/files/<string:file_name>', methods=['DELETE'])
 @token_required
 def delete_file(current_user, file_name):
+    """Удаление файла"""
     file = Files.query.filter(Files.file_name == file_name,
                               Files.user == current_user.name).first()
     if not file:
