d8407f0c11681963cf3d1c70a6697c8c80bdde41
ddanyaa <davidenko.dv@students.dvfu.ru>
2022-01-25 16:35:04 +1000

    [8]add

--- lab8_test.py
+++ lab8_test.py
@@ -1,7 +1,11 @@
-def func(a,b):
-    return a +b
-def func2( c, d):
-    return c- d
+def func(a, b):
+    return a + b
+
+
+def func2(c, d):
+    return c - d
+
+
 if __name__ == "__main__":
-    k=func2(8,4)
-    print(func(k,1))
+    k = func2(8, 4)
+    print(func(k, 1))
